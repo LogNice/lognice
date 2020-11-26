b5c764650847c244385aafe10e56a96bc7338ca3
Omar Aflak <aflakomar@gmail.com>
2020-11-26 16:58:27 +0100

    flask server + cleaned

---
+++ app/app.py
@@ -0,0 +1,46 @@
+import os
+import json
+import base64
+from uuid import uuid4
+from flask import Flask, request
+from prettytable import PrettyTable
+from tasks import evaluate, log_result, summary
+from settings import SESSIONS_PATH, VALIDATOR_NAME
+
+app = Flask(__name__)
+
+def get_error_response(message):
+    return json.dumps({
+        'status': 'error',
+        'message': message
+    })
+
+@app.route('/')
+def hello_world():
+    x = PrettyTable()
+    x.field_names = ['Method', 'Endpoint', 'Parameters', 'Description', 'Return Value']
+    x.add_row(['POST', '/create', 'validator <File(.py)>', 'Creates a new session, provided a validator script for test cases.', 'session_id'])
+    return x.get_html_string()
+
+@app.route('/create', methods=['POST'])
+def create_session():
+    validator_file_key = 'validator'
+    if validator_file_key not in request.files:
+        return get_error_response('No file provided')
+
+    file = request.files[validator_file_key]
+    if file.filename == '':
+        return get_error_response('No file provided')
+
+    if file.filename.split('.')[-1] != 'py':
+        return get_error_response('Wrong file extension')
+
+    session_id = str(uuid4())
+    cwd = os.getcwd()
+    folder_path = os.path.join(cwd, SESSIONS_PATH, session_id)
+    os.makedirs(folder_path)
+    file.save(os.path.join(folder_path, VALIDATOR_NAME))
+    return json.dumps({
+        'status': 'success',
+        'session_id': session_id
+    })
--- app/tasks.py
+++ app/tasks.py
@@ -15,19 +15,19 @@ from settings import (
 
 app = Celery('tasks', backend=CELERY_BACKEND, broker=CELERY_BROKER)
 client = docker.from_env()
-cwd = os.getcwd()
 
 @app.task
 def evaluate(session_id, solution_id):
-    validator_path = os.path.join(cwd, SESSIONS_PATH, session_id, '%s.py' % VALIDATOR_NAME)
+    cwd = os.getcwd()
+    validator_path = os.path.join(cwd, SESSIONS_PATH, session_id, VALIDATOR_NAME)
     solution_path = os.path.join(cwd, SESSIONS_PATH, session_id, '%s.py' % solution_id)
     logs = client.containers.run(EVALUATOR_CONTAINER, volumes={
         validator_path: {
-            'bind': os.path.join(CONTAINER_INPUT_PATH, '%s.py' % VALIDATOR_NAME),
+            'bind': os.path.join(CONTAINER_INPUT_PATH, VALIDATOR_NAME),
             'mode': 'ro'
         },
         solution_path: {
-            'bind': os.path.join(CONTAINER_INPUT_PATH, '%s.py' % SOLUTION_NAME),
+            'bind': os.path.join(CONTAINER_INPUT_PATH, SOLUTION_NAME),
             'mode': 'ro'
         }
     }, auto_remove=True)
