d6a79dc7a6d92fa970819b37add9c6304d2b35c2
Omar Aflak <aflakomar@gmail.com>
2020-12-08 11:42:20 +0100

    summary button

--- app/src/view/home/index.js
+++ app/src/view/home/index.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     const create = document.querySelector('#create')
     const join = document.querySelector('#join')
+    const summary = document.querySelector('#summary')
 
     create.onclick = () => {
         location.href = '/create'
@@ -10,6 +11,14 @@ window.onload = () => {
         bootbox.prompt('Enter session id:', sessionId => {
             if (sessionId) {
                 location.href = `/submit?session-id=${sessionId}`
+            }
+        })
+    }
+
+    summary.onclick = () => {
+        bootbox.prompt('Enter session id:', sessionId => {
+            if (sessionId) {
+                location.href = `/summary?session-id=${sessionId}`
             }
         })
     }
